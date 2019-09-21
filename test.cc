70d4f03faf83bc4bc6ffd82db5d30841220f5172
Bradan J. Wolbeck (CompaqDisc) <admin@compaqdisc.com>
2019-09-21 05:51:47 -0600

    Initial Commit

---
+++ test.cc
@@ -0,0 +1,53 @@
+#include "cdm/Vector2.h"
+#include <iostream>
+#include <cassert>
+
+int main(void)
+{
+	std::cout << "[Type Construction]" << std::endl;
+	{
+		// Test default construction.
+		std::cout << "\tDefault:\t";
+
+		cdm::Vector2<float> vDefault;
+		assert(vDefault.x == 0 && vDefault.y == 0);
+
+		std::cout << "passed." << std::endl;
+		// End Test
+
+		// Test explicit construction.
+		std::cout << "\tExplicit:\t";
+
+		cdm::Vector2<float> vExplicit(3.14159f, 2.71828f);
+		assert(vExplicit.x == 3.14159f && vExplicit.y == 2.71828f);
+
+		std::cout << "passed." << std::endl;
+		// End Test
+	}
+
+	// Test arithmetic operators
+	std::cout << "[Arithmentic Operators]" << std::endl;
+	{
+		// Test addition.
+		std::cout << "\tAddition:\t";
+
+		cdm::Vector2<float> v1(0.5f, 0.25f);
+		cdm::Vector2<float> v2(0.25f, 0.5f);
+
+		cdm::Vector2<float> v3 = v1 + v2;
+
+		assert(v3.x == 0.75f && v3.y == 0.75f);
+
+		std::cout << "passed." << std::endl;
+		// End Test
+
+		// Test subtraction.
+		//std::cout
+	}
+
+
+	// Test compound assignment operators
+	// Test relational operators
+
+	return 0;
+}
